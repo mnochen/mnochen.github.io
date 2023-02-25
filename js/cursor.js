812d19e7a2039fc077a485f08c58397f1dfe8918
mnochen <122211251+mnochen@users.noreply.github.com>
2023-02-25 12:20:07 +0000

    改了很多东西都不知道有哪些了 Updated By Github Actions

--- js/cursor.js
+++ js/cursor.js
@@ -81,3 +81,42 @@ class Cursor {
     CURSOR = new Cursor();
     // 需要重新获取列表时，使用 CURSOR.refresh()
 })();
+
+// 防抖全局计时器
+let TT = null;    //time用来控制事件的触发
+// 防抖函数:fn->逻辑 time->防抖时间
+function debounce(fn, time) {
+    if (TT !== null) clearTimeout(TT);
+    TT = setTimeout(fn, time);
+}
+// 复制提醒
+document.addEventListener("copy", function () {
+    debounce(function () {
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "哎嘿！复制成功",
+                    message: "若要转载最好保留原文链接哦，给你一个大大的赞！",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success",
+                    duration: 5000
+                });
+            }
+        })
+    }, 300);
+})
+
+// 发现有时会和当前页面重复，加一个判断
+function randomPost() {
+    fetch('/baidusitemap.xml').then(res => res.text()).then(str => (new window.DOMParser()).parseFromString(str, "text/xml")).then(data => {
+        let ls = data.querySelectorAll('url loc');
+        while (true) {
+            let url = ls[Math.floor(Math.random() * ls.length)].innerHTML;
+            if (location.href == url) continue;
+            location.href = url;
+            return;
+        }
+    })
+}
